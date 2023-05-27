7bb771826b50d158a6e7402879975650c50f45b2
David Forrester <david.o.forrester@gmail.com>
2023-05-28 08:47:14 +1000

    created contact us page

--- src/contactPage.js
+++ src/contactPage.js
@@ -1,8 +1,8 @@
 export default function contactPage(RestaurentImage) {
   // content
   const content = document.createElement("div");
-  content.id = "content"
-  document.body.appendChild(content)
+  content.id = "content";
+  document.body.appendChild(content);
 
   // header
   const header = document.createElement("header");
@@ -23,7 +23,46 @@ export default function contactPage(RestaurentImage) {
   content.appendChild(main);
 
   // contact us
-  
+  const div = document.createElement("div");
+  div.id = "contact-us";
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "Contact Us";
+  div.appendChild(h1);
+
+  const p = document.createElement("p");
+  p.textContent = "We would love to hear from you!";
+  div.appendChild(p);
+
+  const form = document.createElement("form");
+  form.action = "";
+  form.method = "post";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.name = "name";
+  input.placeholder = "Your Name";
+  form.appendChild(input);
+
+  const input2 = document.createElement("input");
+  input2.type = "email";
+  input2.name = "email";
+  input2.placeholder = "Your Email";
+  form.appendChild(input2);
+
+  const textarea = document.createElement("textarea");
+  textarea.name = "message";
+  textarea.placeholder = "Your Message";
+  form.appendChild(textarea);
+
+  const input3 = document.createElement("input");
+  input3.type = "submit";
+  input3.value = "Submit";
+  form.appendChild(input3);
+
+  div.appendChild(form);
+
+  main.appendChild(div);
 
   // footer
   const footer = document.createElement("footer");
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import menuPage from "./menuPage.js";
 import contactPage from "./contactPage.js";
 import RestaurentImage from "./restaurant.jpg";
 
-menuPage(RestaurentImage);
+contactPage(RestaurentImage);
